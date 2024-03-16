ec9b3ffdb5b2f9789bb69396ecc16d8e13a7d39e
kurapikasphatbum <yilmazebru2002@gmail.com>
2024-03-16 20:49:41 +0100

    static finding max

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,22 +6,23 @@ Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
 
-static double CalculateAverage(int[] numbers)
-    {
-        if (numbers == null || numbers.Length == 0)
-        {
-            throw new ArgumentException("Array is null or empty.");
-        }
-
+static double CalculateAverage(int[] numbers) {
         int sum = 0;
-        foreach (int num in numbers)
-        {
+        foreach (int num in numbers){
             sum += num;
         }
-
         return (double)sum / numbers.Length;
     }
+
+static int findMax(int[] numbers) {
+    int max = numbers[0];
+    for(int i = 0; i< numbers.Length; i++){
+        if(numbers[i] > max)
+        max = numbers[i];
+    }
+    return max;
+}
         int[] array = { 1, 2, 3, 4, 5 };
-        double average = CalculateAverage(array);
-        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Average: " + CalculateAverage(array));
 Console.WriteLine("Fast forward merge");
+Console.WriteLine("Max: " + findMax(array));
